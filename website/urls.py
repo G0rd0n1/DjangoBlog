507badd320d3f3247aac40c9b80c961aa0a7c4b8
gdindi021 <gdindi021@student.wethinkcode.co.za>
2022-09-09 08:30:28 +0200

    Layout changes and login form changed

--- website/urls.py
+++ website/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,4 +9,4 @@ urlpatterns = [
     path("", include("mySite.urls")),
     path("members/", include("django.contrib.auth.urls")),
     path("members/", include("members.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
